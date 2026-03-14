d64e3d7bb8ade33721185a45d655b44740e53478
Pradeep <shabadpradeep1@gmail.com>
2026-03-14 18:32:54 +0530

    basic

---
+++ ToDoList.js
@@ -0,0 +1,32 @@
+const tableBody = document.getElementById('tbody');
+
+let addButton = document.getElementById("add");
+add.addEventListener('click', () => {
+    title = document.getElementById('title');
+    description = document.getElementById('description');
+    let itemJsonArray;
+    if(localStorage.getItem('itemsJson') == null)   {
+        itemJsonArray = [];
+        itemJsonArray.push([title.value,description.value,"unchecked"]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    }
+    else {
+        itemJsonArrayStr = localStorage.getItem('itemsJson');
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([title.value,description.value,"unchecked"]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    }
+    title.value = null;
+    description.value = null;
+    loadData();
+});
+
+function loadData(){
+    itemJsonArrayStr = localStorage.getItem('itemsJson');
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    let str = "";
+    itemJsonArray.forEach((element,index) => {
+        str += `<tr> <th scope='row'>${index+1}</th> <td>${element[0]}</td> <td>${element[1]}</td></tr>`
+    });
+    tableBody.innerHTML = str;
+}
